cedb304958be301c966ec08c05a273d1186094a9
Ibrohim Isroilov <tursunali777@mail.ru>
2021-07-12 12:47:16 +0900

    second commit

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -2,4 +2,7 @@ module github.com/IbrohimIsroilov/shippy/shippy-service-consignment
 
 go 1.16
 
-require google.golang.org/grpc v1.39.0 // indirect
+require (
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/grpc v1.39.0
+)
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -6,7 +6,7 @@ import (
 	"net"
 	"sync"
 
-	pb "gihub.com/IbrohimIsroilov/shippy/shippy-service-consignment/proto/consignment"
+	pb "github.com/IbrohimIsroilov/shippy/shippy-service-consignment/proto/consignment"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -18,6 +18,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 type Repository struct {
@@ -32,6 +33,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// get all consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -49,6 +55,11 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 
 	// Return matching the "Response" message we created in our protobuf definition
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
